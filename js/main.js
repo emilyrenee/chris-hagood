c8ad9fa782cc4221242ff555683d392e571ccfc7
emilyrenee <hagoodem@gmail.com>
2017-02-23 14:22:55 -0500

    added hover styling to items

--- js/main.js
+++ js/main.js
@@ -1,33 +1,23 @@
-//$(".health p").hide();
 
-//$(".health h3").click(function() {
- //$(".health p").toggle();
-//});
 
+// jQuery(document).ready(function(){
+//   $('.box h3').click(function() {
+//       $(this).next().toggle('slow').hide('.box');
+//       // return false;
+//   }).next().hide();
+// });
+
+var content = ".item";
 
 jQuery(document).ready(function(){
-  $('.box h3').click(function() {
-      $(this).next().toggle('slow');
-      // return false;
-  }).next().hide();
+	$('.box').hover(function() {
+		$( this ).addClass('box-hover');
+  		}, function() {
+   		 $( this ).removeClass('box-hover');
+  	})
+  	// .click(function() {
+  	// 	$(this).addClass('box-clicked').toggle('slow');
+  	// })
 });
 
-//enter rotated chevron once clicked
-// jQuery(document).ready(function(){
-//   $('.coverage-group h3').click(function() {
-//       $(this).next().toggle('slow');
-//       return false;
-//   }).next().hide().show(".chevron .right");
-// });
-
-//<span class="chevron right"></span>
-
-
-// jQuery(document).ready(function(){
-// 	var content = ("<p>This is extra content that will be displayed</p>")
-// 	if ($(window).width() >= 768){	
-// 		$(".about").append(content);
-// 	}
-// });
-
-
+ 
