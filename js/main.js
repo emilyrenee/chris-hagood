a45b34604442dcb411c10df604ce8f2e863cade0
emilyrenee <hagoodem@gmail.com>
2017-03-03 16:54:05 -0500

    improved toggle on items and added icons to collaspe

--- js/main.js
+++ js/main.js
@@ -20,6 +20,8 @@ var child = ".item";
 jQuery(document).ready(function(){
 	$('.box').click(function(){
 		$(this).children().slideToggle('slow');
+		$(this).toggleClass('box-expanded');
+		//add function that will expand element to full size of div and hide other boxes
 		// $(this).removeClass('box').addClass('box-expanded');
 	});
 });
