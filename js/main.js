660cc92e975c4e14c1570964d62afea7583ba7a5
emilyrenee <hagoodem@gmail.com>
2017-03-10 14:21:45 -0500

    built licenses graph and added bootstrap style sheets

--- js/main.js
+++ js/main.js
@@ -1,27 +1,13 @@
 
-
-
-// var content = ".item";
-
-// jQuery(document).ready(function(){
-// 	$('.box').click(function() {
-// 		console.log('here');
-//   		$(this).slideToggle('slow');
-//   		$(this).addClass("box-clicked").removeClass("box");
-//   		//hide all other boxes
-//   		$('.box').hide();
-//   		$(this).children(content).show();
-//   		$(this).toggle('slow');
-//   	});
-// });
-
-var child = ".item";
-
 jQuery(document).ready(function(){
 	$('.box').click(function(){
 		$(this).children().slideToggle('slow');
 		$(this).toggleClass('box-expanded');
+    	$("html, body").animate({ scrollTop: $(".box-expanded").offset().top }, 300);
+    		return true;
 		//add function that will expand element to full size of div and hide other boxes
 		// $(this).removeClass('box').addClass('box-expanded');
 	});
 });
+
+
