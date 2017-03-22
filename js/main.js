e94d9b9e970146dfb0398e1694e27edf4e343ffb
emilyrenee <hagoodem@gmail.com>
2017-03-22 15:56:55 -0400

    styled modal drop down

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+// //Initialize Carousel
+// $('.carousel').carousel();
+
 jQuery(document).ready(function() {
 	$('.health').click(function() {
 		$('.health-details').show();
@@ -7,29 +10,75 @@ jQuery(document).ready(function() {
 	});
 });
 
-// jQuery(document).ready(function() {
-// 	$('.life').click( function(){
-// 		$('.life-details').show();
-// 	});
+jQuery(document).ready(function() {
+	$('.life').click(function() {
+		$('.life-details').show();
+		$('.dropdown-toggle').click(function() {
+			$('.life-details').hide();
+		});
+	});
+});
 
-// });
+jQuery(document).ready(function() {
+	$('.vision').click(function() {
+		$('.vision-details').show();
+		$('.dropdown-toggle').click(function() {
+			$('.vision-details').hide();
+		});
+	});
+});
 
-// $.getJSON('./products.json', function(data) {
-// 	console.log(data);
-// });
+jQuery(document).ready(function() {
+	$('.dental').click(function() {
+		$('.dental-details').show();
+		$('.dropdown-toggle').click(function() {
+			$('.dental-details').hide();
+		});
+	});
+});
 
+jQuery(document).ready(function() {
+	$('.cancer').click(function() {
+		$('.cancer-details').show();
+		$('.dropdown-toggle').click(function() {
+			$('.cancer-details').hide();
+		});
+	});
+});
 
-// function Product(type, details, rates) {
-// 			this.type = type;
-// 			this.details = details;
-// 			this.rates = rates;
-// 		};
+jQuery(document).ready(function() {
+	$('.disability').click(function() {
+		$('.disability-details').show();
+		$('.dropdown-toggle').click(function() {
+			$('.disability-details').hide();
+		});
+	});
+});
 
+jQuery(document).ready(function() {
+	$('.accidental').click(function() {
+		$('.accidental-details').show();
+		$('.dropdown-toggle').click(function() {
+			$('.accidental-details').hide();
+		});
+	});
+});
 
-// var Health = new Product(
-// 	"Health",
-// 	"Text Here",
-// 	"Rates Here"
-// 	);
+jQuery(document).ready(function() {
+	$('.hospital').click(function() {
+		$('.hospital-details').show();
+		$('.dropdown-toggle').click(function() {
+			$('.hospital-details').hide();
+		});
+	});
+});
 
-// var Vision = new Product();
+jQuery(document).ready(function() {
+	$('.final-expenses').click(function() {
+		$('.final-expenses-details').show();
+		$('.dropdown-toggle').click(function() {
+			$('.final-expenses-details').hide();
+		});
+	});
+});
+
